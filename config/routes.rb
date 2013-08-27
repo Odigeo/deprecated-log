d0382d073b99a6731ed975c480c17ad74a7f05e1
Peter Bengtson <peter@peterbengtson.com>
2013-08-27 13:30:49 +0200

    Updated bundle. Using ocean-rails v1.18.0.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Log::Application.routes.draw do
 
-  get "/alive" => "alive#index"
-
   scope "/v1" do
     get    "log_excerpts/:from/:to" => "log_excerpts#show"
     delete "log_excerpts/:from/:to" => "log_excerpts#destroy"
