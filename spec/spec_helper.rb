f0cb447787eff87bcd3f1d1b4b9806bd4897943a
Peter Bengtson <peter@peterbengtson.com>
2014-06-30 10:03:28 +0200

    Upgraded to Rails 4.1.2.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,8 @@ RSpec.configure do |config|
   
   # Make "FactoryGirl" superfluous
   config.include FactoryGirl::Syntax::Methods
-  
+
+  # RSpec 3 compatibility
+  config.infer_spec_type_from_file_location!
 end
 
