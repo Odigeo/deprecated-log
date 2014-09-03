191742eec0e0efff5ad5db93b56e49241efb8c77
Peter Bengtson <peter@peterbengtson.com>
2014-09-03 04:47:05 +0200

    Filtering password and credentials from the log.

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Log
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :credentials]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
